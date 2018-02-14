ef74b87511a287e7556bc1d967ff2e30a4e3d921
Dante Planterose <dplanterose@gmail.com>
2018-02-14 11:39:30 +0100

    moving on

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  get 'review/new'
+
+  get 'review/create'
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
